4f09eabff74de3e2358ae9da329613e42382b321
kvkaranverma <karan.verma@catalystone.com>
2026-03-23 09:50:29 +0530

    feat: implementing Linked List

---
+++ LinkedList/implementing-linked-list.js
@@ -0,0 +1,92 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null
+        }
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+
+    insert(index, value) {
+        if (index === 0) {
+            this.prepend(value);
+            return this.printList();
+        }
+        
+        if (index >= this.length) {
+            this.append(value);
+            return this.printList();
+        }
+
+        const newNode = new Node(value);
+        const leader = this.traverseToIndex(index - 1);
+        const holdingPointer = leader.next;
+        leader.next = newNode
+        newNode.next = holdingPointer;
+        this.length++;
+        return this.printList();
+    }
+
+    delete(index) {
+        const leader = this.traverseToIndex(index - 1);
+        leader.next = leader.next.next;
+        this.length--;
+        return this.printList();
+    }
+
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+
+    printList () {
+        const array = [];
+        let currentNode = this.head;
+        while(currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+}
+
+const linkedList = new LinkedList(10);
+
+linkedList.append(5);
+linkedList.append(16);
+linkedList.prepend(1);
+linkedList.insert(2, 99);
+linkedList.insert(20, 88);
+linkedList.insert(0, 77);
+linkedList.delete(2);
+
+console.log(linkedList.printList());
